e50c37a352c11a5a06ec0a93bb84adcbca59a993
will3g <willianpaulo_09@hotmail.com>
2019-12-14 23:08:03 -0300

    fix-spots

--- Backend/src/models/Spot.js
+++ Backend/src/models/Spot.js
@@ -9,6 +9,15 @@ const SpotSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }
+}, {
+  toJSON: {
+    virtuals: true, //Toda vez que um spot for convertido em json
+    // calcule os virtuals e coloque junto 
+  }
+});
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +13,7 @@ mongoose.connect('mongodb+srv://Omni:1234@cluster0-rvxaz.mongodb.net/airc&c?retr
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
